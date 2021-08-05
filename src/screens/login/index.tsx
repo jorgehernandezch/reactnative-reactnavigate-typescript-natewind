d0764e8cd1b1f66cd9a1d0b770906d422426d0ee
Jorge Edo. Hernández <jorge@galpha.co>
2021-08-05 14:17:20 -0300

    Button component

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { styles } from './style';
+import { Button } from '../../components/Button';
 
 
 export function Login() {
   return (
     <View style={styles.container}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Text style={styles.text}>Test</Text>
+      <Button/>
     </View>
   );
 }
