91b8c19f1844909a14fecfcd4efaa779dc4ea71a
Jorge Edo. Hernández <jorge@galpha.co>
2021-08-05 21:11:37 -0300

    Style Home Page

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Lato_100Thin, Lato_300Light, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
-import { Login } from './src/screens/login';
+import { Home } from './src/screens/Home';
 import AppLoading from 'expo-app-loading';
 import { Background } from './src/components/Background';
 
@@ -26,7 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Login />
+      <Home/>
     </Background>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet} from "react-native";
+import Constants from 'expo-constants';
 import { theme } from "../../assets/styles/theme";
 
 export const styles = StyleSheet.create({
   container:{
-    flex: 1
+    flex: 1,
+    marginTop: Constants.statusBarHeight + 20
   },
   text:{
     color: theme.colors.principalText,
