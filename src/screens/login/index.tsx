6d2e89128151c0e993fe08c860fb88344afa7806
jorgehernandezch <jorge@galpha.co>
2023-07-04 10:10:23 -0300

    NativeWInd and Tailwind Lib

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -2,8 +2,8 @@ import { View, Text, Button } from "react-native"
 
 export const Login = ({ navigation }:{navigation:any}) => {
   return(
-    <View>
-      <Text>Login</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-white text-lg">Login</Text>
       <Button title="Entrar" onPress={() => navigation.navigate("Inicio")} />
     </View>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/screens/**/*.tsx",
+    "./src/components/**/*.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
