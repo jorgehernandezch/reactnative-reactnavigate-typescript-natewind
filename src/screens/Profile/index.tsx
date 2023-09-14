526076681b49c2df929355e53414a32ec0962e04
jorgehernandezch <jorge@galpha.co>
2023-09-13 22:54:11 -0300

    Delete theme file

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -3,9 +3,8 @@ import { Text, View } from 'react-native'
 export const Profile = () => {
   return (
     <View className="flex-1 bg-white">
-      <Text className="text-sky-700 text-xl font-bold uppercase mb-6">
-        dsdsdsadskdlksa;ldk;aslkd;laskd;lask;dkaskd;laskd;asldk;laskd;lkaskd;askdasldk;asProfile
-        Page KKLLalaklkalkaloisoaosaiao
+      <Text className="text-sky-700 text-xl font-bold font-[montserratRegular] uppercase mb-6">
+        PROFILE PAGE
       </Text>
     </View>
   )
