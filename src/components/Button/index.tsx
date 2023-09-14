7f856d19b9ae0d5f2b7048f8bad5b370b212dd78
jorgehernandezch <jorge@galpha.co>
2023-09-14 14:17:53 -0300

    Adjusment Button style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { Text, TouchableOpacity, ActivityIndicator, View } from 'react-native'
 import type { ButtonProps } from '../../@types/components'
 
 export const Button = ({
@@ -10,17 +10,19 @@ export const Button = ({
   ...rest
 }: ButtonProps) => {
   return (
-    <TouchableOpacity
-      className={`bg-slate-800 max-h-12 h-12 min-h-[48px] py-2 px-6 rounded-xl flex flex-row justify-center items-center
-        ${extraClasses}
-      `}
-      onPress={onPress}
-      {...rest}
-    >
-      <Text className="font-[latoBold] text-white uppercase">{text}</Text>
-      {loading && (
-        <ActivityIndicator size="small" color="#FFFFFF" className="ml-2" />
-      )}
-    </TouchableOpacity>
+    <View className="flex-1">
+      <TouchableOpacity
+        className={`bg-slate-800 max-h-12 h-12 min-h-[48px] py-2 px-6 rounded-xl flex flex-row justify-center items-center
+          ${extraClasses}
+        `}
+        onPress={onPress}
+        {...rest}
+      >
+        <Text className="font-[latoBold] text-white uppercase">{text}</Text>
+        {loading && (
+          <ActivityIndicator size="small" color="#FFFFFF" className="ml-2" />
+        )}
+      </TouchableOpacity>
+    </View>
   )
 }
